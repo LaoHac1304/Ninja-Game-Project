960f5e186c4afae7aefa7cff41041a5336b2f1fa
obito <simnhankid13042002@gmail.com>
2023-10-30 23:44:45 +0700

    init

---
+++ Assets/Coding/ScrollingBackground.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+    public float scrollSpeed;  // Tốc độ cuộn ban đầu
+    public float jumpScrollSpeed;  // Tốc độ cuộn khi nhảy lên
+    private float originalScrollSpeed;  // Giữ tốc độ cuộn ban đầu
+    private bool isJumping;  // Trạng thái nhảy
+    Vector2 GerakTexture;
+	Renderer gambar;
+
+	// Use this for initialization
+	void Start () {
+        gambar = GetComponent<Renderer>();
+        originalScrollSpeed = scrollSpeed;
+        isJumping = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GerakTexture = new Vector2(Time.time * scrollSpeed, 0);
+
+        float h = Input.GetAxis("Horizontal");
+
+        if (h > 0)
+        {
+            gambar.sharedMaterial.SetTextureOffset("_MainTex", GerakTexture);
+        }
+
+        // Kiểm tra trạng thái nhảy
+        if (isJumping)
+        {
+            scrollSpeed = jumpScrollSpeed;  // Sử dụng tốc độ cuộn khi nhảy
+        }
+        else
+        {
+            scrollSpeed = originalScrollSpeed;  // Sử dụng tốc độ cuộn ban đầu
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+<h1> Game 2D </h1>
+
+<img src="https://ci.appveyor.com/api/projects/status/32r7s2skrgm9ubva?svg=true&passingText=master%20-%20OK" alt="Project Badge">
+
+<p><h2>Game 2D Adventure</h2><p>
+
+
+<img src="http://rachmatgunawan.com/static/media/2d.468e3258.png" />
+
+Demo Youtube :
+https://www.youtube.com/watch?v=Qb0U4UfmjFw
+
+<p>This 2D adventure game has 2 levels. There are several obstacles that must be passed by the player. To be able to go up in each level, players must complete missions, pick up a scroll.
+Players can fire weapons at the enemy, after taking the kunai symbol</p>
+<span style=“color:green;”> 
+Created using:
+<ul>
+  <li>Unity 3D</li>
+<li>c # programming language</li>
+</ul>
+  </span>
+
+<h2><a href="http://nawina2d.com" arget=blank> Demo<a></h2
+
+<h2>CREDITS</h2>
+<h3>ArtS</h3>
+<ul>
+  <li>Erin Facile</li>
+  <li>Clint Belangger</li>
+  <li>Camtatz</li>
+  </ul>
+  
+  <h3>Music</h3>
+<ul>
+  <li>Bart and The Great Hex-a-Hop site</li>
+  <li>A Jingling Sound</li>
+  <li>A Strange Warble</li>
+  <li>Yo Frankie</li>
+  </ul>
+  
+Special Thanks
+
+<p>GG Campus</p>
+<p>Open Game Art</p>
